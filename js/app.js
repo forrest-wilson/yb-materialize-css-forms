2417ca69da203b329b736134f91c20b62f5d0e0b
Forrest Wilson <forrest.wilson@me.com>
2017-11-08 14:54:16 +1300

    Functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+    var obj = {};
 
+    $('select').material_select();
+
+    function myFunction() {
+        $("#myForm input, #myForm select").each(function(i) {
+            var input = $(this);
+
+            alert('Type: ' + input.attr('type') + ' Name: ' + input.attr('name') + ' Value: ' + input.val());
+
+            obj['prop' + i] = input.val();
+        });
+        
+        console.log(obj['prop' + 1], obj);
+    }
+
+    $("#button").on("click", myFunction);
 });
